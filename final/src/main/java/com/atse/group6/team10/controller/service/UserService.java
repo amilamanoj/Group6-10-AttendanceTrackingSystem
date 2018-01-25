b275f1e95f7b0796aebd726a65621e4d478300c0
Tobias Priesching <tobias.prieschingq>
2018-01-25 22:21:34 +0100

    Changes Secure Hashing Algorithm form SHA256 to SHA1

--- final/src/main/java/com/atse/group6/team10/controller/service/UserService.java
+++ final/src/main/java/com/atse/group6/team10/controller/service/UserService.java
@@ -86,7 +86,7 @@ public class UserService {
         PBEKeySpec spec = new PBEKeySpec(password, salt, ITERATIONS, KEY_LENGTH);
         Arrays.fill(password, Character.MIN_VALUE);
         try {
-            SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+            SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
             return skf.generateSecret(spec).getEncoded();
         } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
             throw new AssertionError("Error while hashing a password: " + e.getMessage(), e);
