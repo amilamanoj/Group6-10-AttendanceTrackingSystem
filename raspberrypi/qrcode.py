fe461173020ddbb93bdb8ed8ab669b4d6869249a
Pooja <kulkarpo@github.com>
2018-01-26 13:15:18 +0100

    raspberry pi codes to read-post qrcode
    pending edits -
    update url
    update auth call, postpresented call
    
    Signed-off-by: Pooja <kulkarpo@github.com>

--- raspberrypi/qrcode.py
+++ raspberrypi/qrcode.py
@@ -1,29 +1,49 @@
-#!/usr/bin/python 
+#!/usr/bin/python
 import os, signal, subprocess
-import json
 import requests
-import datetime
+from datetime import datetime
+
+global url
+
+#update the deployment url here
+url = 'https://radiant-land-185414.appspot.com/rest/students/attendances/update/'
+
 
 def read():
-    #note teh time at which the qrcode read starts
-    qrcodetext = ''
-    now = datetime.datetime.now()
     zbarcam=subprocess.Popen("zbarcam --raw --nodisplay /dev/video0", stdout=subprocess.PIPE, shell=True, preexec_fn=os.setsid)
     print "cam successfully started"
+    # wait 2 minutes to scan the qr code else exit
     while True:
         qrcodetext = zbarcam.stdout.readline()
         if qrcodetext != "":
-            print "Success" 
             break
-        if str(datetime.datetime.now()) is str(now + datetime.timedelta(minutes=2)):
-            print "No qrcode scanned in last 2 minutes. Exiting..."
     os.killpg(zbarcam.pid, signal.SIGTERM)
     print "cam successfully stopped"
     return qrcodetext
 
 
-def postattendance(data):
-    url = 'https://radiant-land-185414.appspot.com/rest/students/attendances/update/'+data
+def tutorauth(data):
+    global url
+    #update the link of attendance server here
+    url = url+data
     headers = {'content-type': 'application/json'}
     response = requests.post(url, headers=headers)
-    return response
+    return response.status_code
+
+
+def postattendance(data):
+    global url
+    #update the link of attendance server here
+    url = url+data
+    headers = {'content-type': 'application/json'}
+    response = requests.post(url, headers=headers)
+    return response.status_code
+
+
+def postpresented(data):
+    global url
+    # update the link of attendance server here
+    url = url+data
+    headers = {'content-type': 'application/json'}
+    response = requests.post(url, headers=headers)
+    return response.status_code
--- raspberrypi/read_qrcode.py
+++ raspberrypi/read_qrcode.py
@@ -3,37 +3,103 @@
 import qrcode
 import pifacecad
 import sys
+import time
 
 cad = pifacecad.PiFaceCAD()
 
+def tutorAuthentication():
+    cad.lcd.clear()
+    cad.lcd.write("Tutor Authentication")
+    cad.lcd.set_cursor(1, 0)
+    cad.lcd.write('Waiting for QR')
+    result = qrcode.read().strip()
+    if not result:
+        cad.lcd.set_cursor(1, 0)
+        cad.lcd.write("NO QR code found")
+        sys.exit()
+    cad.lcd.set_cursor(1, 0)
+    cad.lcd.write(result)
+
+    # call auth API for tutor here
+    status = qrcode.tutorauth(result)
+
+    cad.lcd.set_cursor(1, 0)
+    if '200' == status:
+        cad.lcd.write("AUTH SUCCESS")
+        return
+    else:
+        cad.lcd.write("AUTH FAILURE.exiting...")
+        sys.exit()
+
 def main():
+    # tutor auth
+    tutorAuthentication()
+
     while True:
         cad.lcd.clear()
-        cad.lcd.write('Waiting for QRcode')
-        #Read QR code
-        result = qrcode.read().strip()
-        if not result:
-            cad.lcd.write('No QRcode found. Exiting... ')
-            sys.exit()
-        cad.lcd.clear()
-        cad.lcd.write(result)
+        #prompt the tutor to mark attendance or presented status of student
+        cad.lcd.write('Press 1 attendance')
+        #switch to next line
+        cad.lcd.set_cursor(1, 0)
+        cad.lcd.write('Press 2 presentation')
 
-        '''
-        TO-DO before calling the REST API to update specific student's attendence
-        prompt the tutor to enter if the student has presented.
-    
-        if a student has presented before then prompting tutor repeatedly upcoming
-        weeks should be avoided.
-        '''
-        #call to backend
-        status = qrcode.postattendance(result)
-        cad.lcd.clear() 
-        if '200' in status:
-            cad.lcd.write('SUCCESS')
-        else:
-            cad.lcd.write('FAILURE')
-        break
+        #wait for tutor input
+        if cad.switches[1].value == 1:
+            cad.lcd.clear()
+            # prompt what to do when done marking attendance
+            cad.lcd.write('Press 3 when done')
+            while True:
+                if cad.switches[3].value == 1:
+                    break
+                cad.lcd.set_cursor(1, 0)
+                cad.lcd.write('Waiting for QRcode')
 
+                #Read QR code
+                result = qrcode.read().strip()
+                if not result:
+                    cad.lcd.clear()
+                    cad.lcd.write('No QRcode found. Exiting... ')
+                cad.lcd.set_cursor(1, 0)
+                cad.lcd.write(result)
+
+                # call to backend to post attendance
+                status = qrcode.postattendance(result)
+
+                cad.lcd.set_cursor(1, 0)
+                if '200' == status:
+                    cad.lcd.write('SUCCESS')
+                else:
+                    cad.lcd.write('FAILURE')
+                #sleep 2 seconds after prompting user about the SUCCESS/ FAILURE message
+                time.sleep(2)
+
+        if cad.switches[2].value == 1:
+            cad.lcd.clear()
+            # prompt what to do when done marking Presented status
+            cad.lcd.write('Press 3 when done')
+            while True:
+                if cad.switches[3].value == 1:
+                    break
+                cad.lcd.set_cursor(1, 0)
+                cad.lcd.write('Waiting for QRcode')
+
+                # Read QR code
+                result = qrcode.read().strip()
+                if not result:
+                    cad.lcd.clear()
+                    cad.lcd.write('No QRcode found. Exiting... ')
+                cad.lcd.set_cursor(1, 0)
+                cad.lcd.write(result)
+
+                # call to backend to post presented
+                status = qrcode.postpresented(result)
+
+                cad.lcd.set_cursor(1, 0)
+                if '200' == status:
+                    cad.lcd.write('SUCCESS')
+                else:
+                    cad.lcd.write('FAILURE')
+                time.sleep(2)
 
 
 if __name__ == "__main__":
